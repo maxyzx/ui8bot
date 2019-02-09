31d763022791e9a4ac2133a5314bdbac08d163e8
Manh Nguyen <manhng.201@gmail.com>
2019-02-09 18:01:50 +0700

    Add data analytics

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -3,5 +3,6 @@ class AnalyticsController < ApplicationController
     @total_products = Product.all.size
     @total_downloads = Product.where(download: true).size
     @categories = Category.all
+    @estimate_month_time_to_download = (@total_products - @total_downloads)/20
   end
 end
