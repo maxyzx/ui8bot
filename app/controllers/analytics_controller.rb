c14321b90e2668fe252f5f78d4bc165540f95eb0
Max <nguyen.ba.manh@moneyforward.vn>
2020-05-02 02:09:24 +0700

    removes analytics page

--- app/controllers/analytics_controller.rb
+++
@@ -1,8 +0,0 @@
-class AnalyticsController < ApplicationController
-  def index
-    @total_products = Product.all.size
-    @product_downloads = Product.where(download: true).order(:download_at)
-    @categories = Category.all
-    @estimate_month_time_to_download = (@total_products - @product_downloads.size)/20
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   root to: 'products#index'
 
-  get :analytics, to: "analytics#index"
-
   resources :products
   resources :product_files
   resources :categories
